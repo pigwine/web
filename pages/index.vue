0e82a50d63c517f41e2a630ac6c28fe40270633b
kaima <18262538266@163.com>
2024-11-10 10:27:32 +0800

    my first class

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="flex flex-col items-center justify-center min-h-screen">
+            <h1 class="text-4xl font-bold mb-8">首页</h1>
+            <UButton
+                label="立即订阅"
+                size="lg"
+                @click="handleClick"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup>
+const handleClick = () => {
+    window.open('https://x.com/nanjingsifang', '_blank')
+}
+</script>
